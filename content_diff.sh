3d38d65960d085575a2da598a523224b1c7726ab
seangarvan <38312899+seangarvan@users.noreply.github.com>
2019-06-14 15:05:25 +0100

    Update content_diff.sh

--- content_diff.sh
+++ content_diff.sh
@@ -8,34 +8,40 @@ email_addr='a@a.com
 
 wget -q -O $BASEDIR/$playlist_input $playlist_url 
 
-more $BASEDIR/$playlist_input | grep "#EXTINF:" | awk -F'"' '{ print $8$9 }' > $BASEDIR/$playlist_new
+if [ "$?" -eq 0 ]; then
 
-sort $BASEDIR/$playlist_new -o $BASEDIR/$playlist_new
+  more $BASEDIR/$playlist_input | grep "#EXTINF:" | awk -F'"' '{ print $8$9 }' > $BASEDIR/$playlist_new
 
-rm $BASEDIR/$playlist_input
+  sort $BASEDIR/$playlist_new -o $BASEDIR/$playlist_new
 
-if [ ! -f $BASEDIR/$playlist_curr ];
-then
-   cp $BASEDIR/$playlist_new $BASEDIR/$playlist_curr
+  rm $BASEDIR/$playlist_input
+
+  if [ ! -f $BASEDIR/$playlist_curr ]; then
+    cp $BASEDIR/$playlist_new $BASEDIR/$playlist_curr
+  fi
+
+  sort $BASEDIR/$playlist_curr -o $BASEDIR/$playlist_curr
+
+  playlist_additions="$(diff -w "$BASEDIR/$playlist_new" "$BASEDIR/$playlist_curr" | grep '<' | sed 's/< //' )"
+
+  playlist_deletions="$(diff -w "$BASEDIR/$playlist_new" "$BASEDIR/$playlist_curr" | grep '>' | sed 's/> //' )"
+
+  if [[ ! -z $playlist_additions ]]; then
+    echo "$playlist_additions" | mail -s "New IPTV Content $(date +"%Y%m%d")" $email_addr
+  fi
+
+  if [[ ! -z $playlist_deletions ]]; then
+    echo "$playlist_deletions" | mail -s "Removed IPTV Content $(date +"%Y%m%d")" $email_addr
+  fi
+
+  mv $BASEDIR/$playlist_curr $BASEDIR/"$playlist_curr"_$(date +"%Y%m%d")
+
+  mv $BASEDIR/$playlist_new $BASEDIR/$playlist_curr
+
+  find $BASEDIR/ -type f -mtime +7 -name '*.txt*' -execdir rm -- '{}' \;
+  
+else
+  
+  echo "Unable to Download IPTV Playlist $(date +"%Y%m%d")" | mail -s "Unable to Download IPTV Playlist $(date +"%Y%m%d")" $email_addr
+
 fi
-
-sort $BASEDIR/$playlist_curr -o $BASEDIR/$playlist_curr
-
-playlist_additions="$(diff -w "$BASEDIR/$playlist_new" "$BASEDIR/$playlist_curr" | grep '<' | sed 's/< //' )"
-
-playlist_deletions="$(diff -w "$BASEDIR/$playlist_new" "$BASEDIR/$playlist_curr" | grep '>' | sed 's/> //' )"
-
-if [[ ! -z $playlist_additions ]]; then
-  echo "$playlist_additions" | mail -s "New IPTV Content $(date +"%Y%m%d")" $email_addr
-fi
-
-if [[ ! -z $playlist_deletions ]]; then
-  echo "$playlist_deletions" | mail -s "Removed IPTV Content $(date +"%Y%m%d")" $email_addr
-fi
-
-mv $BASEDIR/$playlist_curr $BASEDIR/"$playlist_curr"_$(date +"%Y%m%d")
-
-mv $BASEDIR/$playlist_new $BASEDIR/$playlist_curr
-
-find $BASEDIR/ -type f -mtime +7 -name '*.txt*' -execdir rm -- '{}' \;
-
